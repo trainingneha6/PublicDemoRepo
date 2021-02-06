7ac310db6bc5408e1c0aa239ecbbdc9a99fa7745
trainingneha6 <75961988+trainingneha6@users.noreply.github.com>
2021-02-05 20:33:00 -0600

    Update simpleJavaCode.java
    Updated the code

--- simpleJavaCode.java
+++ simpleJavaCode.java
@@ -3,7 +3,8 @@ public class simpleJavaCode {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Hello!!! This is my simple Java Code.");
+		System.out.println("Hello!!! This is my First Java Code.");
+		System.out.println("Hello!!! This is my Second Java Code.");
 
 	}
 
